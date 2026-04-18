d3acb63f0cd51598bb01b11a0a33ca3dfc33ba36
Omar Ahmed <152551143+omar27105@users.noreply.github.com>
2026-04-18 02:36:36 +0200

    sp_unit done memory_stage not done yet
    testing for sp_unit hasn't been done yet

--- src/rtl/components/sp_unit.vhd
+++ src/rtl/components/sp_unit.vhd
@@ -1,18 +1,46 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+
 
 ENTITY sp_unit IS
+    generic (
+        STACK_START_ADDR : STD_LOGIC_VECTOR(31 DOWNTO 0) := X"00000FFF"
+    );
     PORT (
         clk : IN STD_LOGIC;
         reset : IN STD_LOGIC;
         sp_write_en : IN STD_LOGIC;
-        is_push : IN STD_LOGIC;
+        SP_OP : IN STD_LOGIC; --SP_OP in the schematic
         sp_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END ENTITY sp_unit;
 
 ARCHITECTURE rtl OF sp_unit IS
+SIGNAL sp_reg : STD_LOGIC_VECTOR(31 DOWNTO 0);
 BEGIN
-    -- TODO: implement stack pointer update policy
-    -- TODO: initialize SP to (2^12 - 1)
+-- TODO: implement stack pointer update policy
+-- TODO: initialize SP to (2^12 - 1)
+-- process(clk, reset)
+--     begin
+--         if reset = '1' then
+--             sp_reg <= STACK_START_ADDR;
+--         elsif rising_edge(clk) then
+--             if sp_write_en = '1' then
+--                 if SP_OP = '0' then
+--                     sp_reg <= std_logic_vector(unsigned(sp_reg) - 4);
+--                 else
+--                     sp_reg <= std_logic_vector(unsigned(sp_reg) + 4);
+--                 end if;
+--             end if;
+--         end if;
+--     end process;
+sp_reg <= std_logic_vector(unsigned(sp_reg) - 4) when (sp_write_en = '1' and SP_OP = '0') else
+          std_logic_vector(unsigned(sp_reg) + 4) when (sp_write_en = '1' and SP_OP = '1') else
+          STACK_START_ADDR when (reset = '1' AND sp_write_en = '1') else
+          sp_reg;
+
+
+    sp_out <= sp_reg;
 END ARCHITECTURE rtl;
--- src/rtl/stages/5-memory/src/memory_stage.vhd
+++ src/rtl/stages/5-memory/src/memory_stage.vhd
@@ -33,11 +33,66 @@ ENTITY memory_stage IS
         wb_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
         reg_write_address_out : OUT reg_idx_t;
         pc_write_en_out : OUT STD_LOGIC;
-        pc_write_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        pc_write_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+        PC_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END ENTITY memory_stage;
 
 ARCHITECTURE rtl OF memory_stage IS
+SIGNAL mem_addr : STD_LOGIC_VECTOR(11 DOWNTO 0);
+SIGNAL mem_data_in : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL mem_data_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL MEMORY_READ_EN : STD_LOGIC;
+SIGNAL MEMORY_WRITE_EN : STD_LOGIC;
+SIGNAL mem_address : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL interrupt_addr_sign_extend : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL CCR_FLAGS_SIGN_EXTEND : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL SP_PUSH : STD_LOGIC_VECTOR(31 DOWNTO 0);
+SIGNAL SP_POP : STD_LOGIC_VECTOR(31 DOWNTO 0);
 BEGIN
-    -- Definition-only skeleton.
+-- Definition-only skeleton.
+    UPDATE_FLAGS_OUT <= UPDATE_FLAGS_IN;
+    OUTPUT_PORT_EN_OUT <= OUTPUT_PORT_EN_IN;
+    REG_WB_EN_OUT <= REG_WB_EN_IN;
+    CCR_FLAGS_SIGN_EXTEND <= (31 downto 3 => corrected_ccr_flags_in(2)) & corrected_ccr_flags_in;
+    interrupt_addr_sign_extend <= (31 downto 2 => interrupt_adr_in(1)) & interrupt_adr_in;
+    mem_component : ENTITY work.memory
+        PORT MAP (
+            clk => clk,
+            mem_addr => mem_addr,
+            mem_data_in => mem_data_in,
+            mem_data_out => mem_data_out,
+            MEMORY_READ_EN => MEMORY_READ_EN,
+            MEMORY_WRITE_EN => MEMORY_WRITE_EN
+        );
+    mux_mem_addr : ENTITY work.mux_4to1
+        GENERIC MAP (
+            WIDTH => 32
+        )
+        PORT MAP (
+            sel => MEM_ADDRESS_SEL_IN,
+            input_0 => alu_result_in,
+            input_1 => interrupt_addr_sign_extend,
+            input_2 => SP_PUSH, 
+            input_3 => SP_POP, 
+            output => mem_address
+        );
+    mux_data_in : ENTITY work.mux_4to1
+        GENERIC MAP (
+            WIDTH => 32
+        )
+        PORT MAP (
+            sel => MEM_WRITE_SEL_IN,
+            input_0 => alu_result_in,
+            input_1 => next_pc_in,
+            input_2 => CCR_FLAGS_SIGN_EXTEND,
+            input_3 => (others => '0'),
+            output => mem_data_in
+        );
+    MEMORY_READ_EN <= MEMR_IN WHEN FETCH_MEM_SEL = '1' ELSE '1';
+    MEMORY_WRITE_EN <= MEMW_IN WHEN FETCH_MEM_SEL = '1' ELSE '0';
+    mem_addr <= PC_in(11 DOWNTO 0) WHEN FETCH_MEM_SEL = "0" ELSE
+                mem_adr_in(11 DOWNTO 0) WHEN FETCH_MEM_SEL = "1";
+    --TODO :: SP UNIT missing to be implemented with fixes and revising for the code
 END ARCHITECTURE rtl;
