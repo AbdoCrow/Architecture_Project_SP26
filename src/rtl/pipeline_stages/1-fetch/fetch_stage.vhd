82356f5aee59f9369a3546619d981b5fec22015b
Ahmed Salah <ahmed10salah111@gmail.com>
2026-05-09 13:31:42 +0300

    fix: update definitions of all stages

--- src/rtl/pipeline_stages/1-fetch/fetch_stage.vhd
+++ src/rtl/pipeline_stages/1-fetch/fetch_stage.vhd
@@ -7,32 +7,34 @@ ENTITY fetch_stage IS
         clk : IN STD_LOGIC;
         reset : IN STD_LOGIC;
 
-        PC_ENABLE : IN STD_LOGIC;
+        PC_WRITE_ENABLE : IN STD_LOGIC;
         FETCH_STALL : IN STD_LOGIC;
         MULTICYCLE_STALL : IN STD_LOGIC;
+        MULTICYCLE_SEL : IN multicycle_sel_t;
         FLUSH : IN STD_LOGIC;
 
         CORRECT_PC : IN STD_LOGIC;
         correct_pc_value : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-        instruction_word_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        fetched_instruction_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        loaded_pc_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-        predictor_update_en : IN STD_LOGIC;
-        predictor_update_pc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        predictor_update_taken : IN STD_LOGIC;
+        DECODE_INT_TARGET_IDX : IN int_idx_t;
+
+        COND_BRANCH : IN STD_LOGIC;
+        BRANCH_TAKEN : IN STD_LOGIC;
+        HLT : IN STD_LOGIC;
+        FETCH_MEMORY_HAZARD : IN STD_LOGIC;
+        ALLOW_HW_INT: IN STD_LOGIC;
 
         pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
         next_pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        instr_address_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        branch_prediction_out : OUT STD_LOGIC;
-        cond_branch_hint_out : OUT STD_LOGIC;
-        uncond_branch_hint_out : OUT STD_LOGIC
+        instr_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        branch_prediction_out : OUT STD_LOGIC
     );
 END ENTITY fetch_stage;
 
 ARCHITECTURE rtl OF fetch_stage IS
 BEGIN
-    -- Definition-only skeleton.
-    -- TODO: use instruction_word_in(BR_HINT_COND_BIT/BR_HINT_UNCOND_BIT) for early decode.
-    -- TODO: connect dynamic branch predictor update and prediction outputs.
+-- include pc reg, multicycle instruction unit and branch prediction unit
 END ARCHITECTURE rtl;
--- src/rtl/pipeline_stages/5-memory/memory_stage.vhd
+++ src/rtl/pipeline_stages/5-memory/memory_stage.vhd
@@ -10,39 +10,33 @@ ENTITY memory_stage IS
         reset : IN STD_LOGIC;
 
         LOAD_FLAGS_IN : IN STD_LOGIC;
-        PC_WRITE_EN_IN : IN STD_LOGIC;
         MEM_WRITE_SEL_IN : IN mem_write_sel_t;
         MEMW_IN : IN STD_LOGIC;
         MEMR_IN : IN STD_LOGIC;
-        UPDATE_FLAGS_IN : IN STD_LOGIC;
         MEM_ADDRESS_SEL_IN : IN mem_address_sel_t;
-        OUTPUT_PORT_EN_IN : IN STD_LOGIC;
-        REG_WB_EN_IN : IN STD_LOGIC;
+        HLT : IN STD_LOGIC;
 
         corrected_ccr_flags_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
         alu_flags_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        alu_result_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        alu_result_1_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         mem_adr_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        interrupt_adr_in : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        reg_write_address_in : IN reg_idx_t;
+        interrupt_adr_in : IN int_idx_t;
         next_pc_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-        UPDATE_FLAGS_OUT : OUT STD_LOGIC;
-        OUTPUT_PORT_EN_OUT : OUT STD_LOGIC;
-        REG_WB_EN_OUT : OUT STD_LOGIC;
+        mem_read_data_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
         flag_wb_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-        wb_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        reg_write_address_out : OUT reg_idx_t;
-        pc_write_en_out : OUT STD_LOGIC;
-        pc_write_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        wb_data_1_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
 
+        mem_address : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        mem_write_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        MEMORY : OUT STD_LOGIC
         -- ADDED FROM OMAR
-        PC_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        FETCH_MEM_SEL : IN STD_LOGIC;
-        HLT : IN STD_LOGIC;
-        loaded_pc : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        fetched_instr : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        -- PC_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        -- FETCH_MEM_SEL : IN STD_LOGIC;
+        -- loaded_pc : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        -- fetched_instr : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        -- pc_write_data_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 
     );
 END ENTITY memory_stage;
@@ -62,9 +56,6 @@ SIGNAL sp_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
 SIGNAL WB_DATA : STD_LOGIC_VECTOR(31 DOWNTO 0);
 BEGIN
 -- Definition-only skeleton.
-    UPDATE_FLAGS_OUT <= UPDATE_FLAGS_IN;
-    OUTPUT_PORT_EN_OUT <= OUTPUT_PORT_EN_IN;
-    REG_WB_EN_OUT <= REG_WB_EN_IN;
     CCR_FLAGS_SIGN_EXTEND <= (31 downto 3 => corrected_ccr_flags_in(2)) & corrected_ccr_flags_in;
     interrupt_addr_sign_extend <= (31 downto 2 => interrupt_adr_in(1)) & interrupt_adr_in;
     mem_component : ENTITY work.memory
@@ -82,7 +73,7 @@ BEGIN
         )
         PORT MAP (
             sel => MEM_ADDRESS_SEL_IN,
-            input_0 => alu_result_in,
+            input_0 => alu_result_1_in,
             input_1 => interrupt_addr_sign_extend,
             input_2 => SP_PUSH, 
             input_3 => SP_POP, 
@@ -94,7 +85,7 @@ BEGIN
         )
         PORT MAP (
             sel => MEM_WRITE_SEL_IN,
-            input_0 => alu_result_in,
+            input_0 => alu_result_1_in,
             input_1 => next_pc_in,
             input_2 => CCR_FLAGS_SIGN_EXTEND,
             input_3 => (others => '0'),
@@ -119,9 +110,9 @@ BEGIN
     SP_POP <= sp_out;
     SP_PUSH <= STD_LOGIC_VECTOR(unsigned(sp_out) + 4);     
 
-    WB_DATA <= alu_result_in WHEN MEMR_IN = '0' ELSE mem_data_out;
+    WB_DATA <= alu_result_1_in WHEN MEMR_IN = '0' ELSE mem_data_out;
     flag_wb_out <= alu_flags_in WHEN LOAD_FLAGS_IN = '0' ELSE WB_DATA(2 DOWNTO 0);
-    wb_data_out <= WB_DATA;
+    wb_data_1_out <= WB_DATA;
 
     loaded_pc <= mem_data_out;
     fetched_instr <= mem_data_out;
