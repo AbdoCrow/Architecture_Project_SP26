475df404b351d9c6b69663ea8e031372ccb68881
Ahmed Salah <ahmed10salah111@gmail.com>
2026-05-10 22:30:13 +0300

    tests

--- testcases/test2.asm
+++ testcases/test2.asm
@@ -12,18 +12,18 @@
 900
 
 .ORG 900 #this is hw int
-IN R7 		 #R7=6
+IN R7 		 #R7=5
 AND R0,R0,R0     #N=0,Z=1
-OUT R7
+OUT R3
 RTI              #POP PC and flags restored
-ADD R1, R2, R3  #Try Hardware interrupt when fetching this (in a second run) - infinite loop?
+IADD R1, R2, R3  #Try Hardware interrupt when fetching this (in a second run) - infinite loop?
 
 .ORG 10
-IN R1            #R1=50
-IN R2            #R2=30
-IN R3            #R3=300
-IN R4            #R4=100
-PUSH R3          #SP=FFE, M[FFF]=300
+IN R1            #R1=30
+IN R2            #R2=50
+IN R3            #R3=100
+IN R4            #R4=300
+Push R4          #SP=FFE, M[FFF]=300
 JMP 30           #taken
 INC R1	         #this statement shouldn't be executed
  
@@ -38,24 +38,24 @@ SETC             #this statement shouldn't be executed, C-->1
 JZ 30            #shouldn't be taken
 
 # Check on flag updated on ALU operations
-NOT R5           #R5=FFFF_FFFF, Z=0, C-->no change, N=1
-SUB R6, R5, R5   #Z=1, N=0
-IN R1            #R1=55
+NOT R5           #R5=FFFF_FFFF, Z= 0, C--> not change, N=1
+SUB R7, R5, R5   #Z=1, N=0
+IN R1            #R1=60
 JZ 60            #jump taken, Z=0
 ADD R1, R2, R3   #should not be executed
 
 .ORG 60
-IN R1            #R1=75
+IN R1            #R1=70
 JZ 70            #jump not taken
 JMP 70           #jump taken & TEST prediction=false then unconditional jump
 inc r1           #should not be executed 
 
 # Load use
 .ORG 70
-IADD R1, R1, 10  #R1=85
+IADD R1, R1, 10  #R1=80
 SUB R7, R1, R1   #Z=1, N=0
-PUSH R1          #SP=FFD, M[FFE]=85
-POP R1           #SP=FFE, R1=85
+PUSH R1          #SP=FFD, M[FFE]=80
+POP R1           #SP=FFE, R1=80
 JZ 80            #Taken
 INC R1 		 #try hardware interrupt when fetching this
 
@@ -66,19 +66,19 @@ JMP 700          #jump taken
 
 #check on load use
 .ORG 700
-ADD R7, R0, R1   #R7=85
+ADD R7, R0, R1   #R7=80
 POP R6           #R6=300, SP=FFF, try hardware interrupt here
 Call 300         #SP=FFE, M[FFF]=next PC
-INC R6	         #R6=601, this statement shouldn't be executed till call returns, C--> 0, N-->0, Z-->0
+INC R6	         #R6=401, this statement shouldn't be executed till call returns, C--> 0, N-->0, Z-->0
 NOP
 NOP
 HLT
 
 .ORG 300
-ADD R6,R3,R6     #R6=600
-ADD R1,R1,R2     #R1=B5, C->0, N=0, Z=0
+ADD R6,R3,R6     #R6=400
+ADD R1,R1,R2     #R1=D0, C->0, N=0, Z=0
 RET
-ADD R1, R1, R1   #this should not be executed - try hardware interrupt when this is at fetch
+ADD R1, R1, R1   #this shouldnot be executed - try hardware interrupt when this is at fetch
 
 .ORG 500
 NOP
