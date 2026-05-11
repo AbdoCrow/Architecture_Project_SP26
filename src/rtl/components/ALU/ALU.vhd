96cb9a86f18b2d98105c4b4c7d6e3ccc823a4886
Ahmed Salah <ahmed10salah111@gmail.com>
2026-05-11 16:11:38 +0300

    fix ALU file

--- src/rtl/components/ALU/ALU.vhd
+++ src/rtl/components/ALU/ALU.vhd
@@ -2,21 +2,6 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 USE work.isa_defs_pkg.ALL;
-ENTITY ALU IS
-    PORT (
-        A       : IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
-        B       : IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
-        prev_flags : IN  STD_LOGIC_VECTOR(2 DOWNTO 0);
-        output_flags : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-        ALUOP   : IN  alu_op_t;
-        Result1  : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        Result2  : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
-    );
-END ALU;
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
-USE work.isa_defs_pkg.ALL;
 
 ENTITY ALU IS
     PORT (
